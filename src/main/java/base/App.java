03e0ce3eba171c10d4358af0fb722d90c658a74c
Alice Yu <alicesyu2002@gmail.com>
2021-06-07 00:01:26 -0400

    Minor spelling error fix

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -89,24 +89,24 @@ public class App {
             }
             else
             {
-                String failStart = myApp.readInputString("(Does the car crank up but fail to start? ");
+                String failStart = myApp.readInputString("Does the car crank up but fail to start? ");
                 if (failStart.equals("y"))
                 {
                     System.out.println("Check spark plug connections.\n");
                 }
                 else
                 {
-                    String startUp = myApp.readInputString("(Does the engine start and then die? ");
+                    String startUp = myApp.readInputString("Does the engine start and then die? ");
                     if (startUp.equals("y"))
                     {
-                        String fuel = myApp.readInputString("(Does you car have fuel injection? ");
+                        String fuel = myApp.readInputString("Does you car have fuel injection? ");
                         if(fuel.equals("y"))
                         {
                             System.out.println("Get it in for service.\n");
                         }
                         else
                         {
-                            System.out.println("  Check to ensure the choke is opening and closing.");
+                            System.out.println("Check to ensure the choke is opening and closing.");
 
                         }
                     }
@@ -123,8 +123,7 @@ public class App {
     public String readInputString(String input)
     {
         System.out.print(input);
-        String string = in.next();
-        return string;
+        return in.next();
     }
 
 
